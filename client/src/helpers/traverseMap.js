79120ebfd6e4f5f2f053209ec4a19ad97483cb7a
Jeff Lapp <lappjeff20@gmail.com>
2019-10-22 21:04:47 -0400

    Optimized some traversal timing
    Improved readablity and performance of traverse by adding helper wait function

--- client/src/helpers/traverseMap.js
+++ client/src/helpers/traverseMap.js
@@ -2,20 +2,19 @@
  * Dependencies
  */
 
-import axiosWithAuth from './axiosWithAuth';
-// import writeRoomJson from './writeRoomJson';
+import axiosWithAuth from "./axiosWithAuth";
 
 /**
  * Constants
  */
 
-const baseUrl = 'https://lambda-treasure-hunt.herokuapp.com';
+const baseUrl = "https://lambda-treasure-hunt.herokuapp.com";
 const reverseDirection = {
-  'n': 's',
-  's': 'n',
-  'e': 'w',
-  'w': 'e',
-}
+  n: "s",
+  s: "n",
+  e: "w",
+  w: "e"
+};
 
 /**
  * Define Helpers
@@ -36,67 +35,68 @@ const reverseDirection = {
  * Define traversal algorithm
  */
 
+//  returns a promise that can be used to halt something for an allotted amount of time
+const wait = ms => {
+  return new Promise((res, rej) => setTimeout(res, ms));
+};
+
 async function traverseMap() {
-  const traversalGraph = {}
-  const stack = []
+  const traversalGraph = {};
+  const stack = [];
 
   try {
     // Get current room information
-    const response = await axiosWithAuth().get(`${baseUrl}/api/adv/init/`)
-  
-    let currentRoomID = response.data.room_id
-    traversalGraph[currentRoomID] = response.data
+    const response = await axiosWithAuth().get(`${baseUrl}/api/adv/init/`);
+
+    let currentRoomID = response.data.room_id;
+    traversalGraph[currentRoomID] = response.data;
     for (let i = 0; i < response.data.exits.length; i++) {
-      traversalGraph[currentRoomID][response.data.exits[i]] = "?"
+      traversalGraph[currentRoomID][response.data.exits[i]] = "?";
     }
 
-    // DFS until dead end is hit
-    let count = 0
-    const interval = setInterval(async () => {
-      if (Object.keys(traversalGraph).length === 500 ) {
-        clearInterval(interval)
-      } else {
-        let prevRoomID = currentRoomID
-        let prevRoom = traversalGraph[prevRoomID]
+    await wait(response.data.cooldown * 1000);
 
-        const unexploredExits = []
-        const exits = prevRoom.exits
-        for (let i = 0; i < exits.length; i++) {
-          if (prevRoom[exits[i]] === '?') {
-            unexploredExits.push(exits[i])
-          }
+    let count = 0;
+    while (Object.keys(traversalGraph).length <= 2) {
+      let prevRoomID = currentRoomID;
+      let prevRoom = traversalGraph[prevRoomID];
+
+      const unexploredExits = [];
+      const exits = prevRoom.exits;
+      for (let i = 0; i < exits.length; i++) {
+        if (prevRoom[exits[i]] === "?") {
+          unexploredExits.push(exits[i]);
         }
-
-        let direction
-        if (unexploredExits.length > 0) {
-          direction = unexploredExits.pop()
-          stack.push(reverseDirection[direction])
-        } else {
-          direction = stack.pop()
-        }
-
-        const moveRes = await axiosWithAuth().post(`${baseUrl}/api/adv/move/`, {
-          "direction": direction
-        })
-        currentRoomID = moveRes.data.room_id;
-        if (!(currentRoomID in traversalGraph)) {
-          traversalGraph[currentRoomID] = moveRes.data
-          for (let i = 0; i < moveRes.data.exits.length; i++) {
-            traversalGraph[currentRoomID][moveRes.data.exits[i]] = "?"
-          }
-        }
-        traversalGraph[prevRoomID][direction] = currentRoomID
-        traversalGraph[currentRoomID][reverseDirection[direction]] = prevRoomID
-
-        count += 1
-
-        console.log('traversalGraph', traversalGraph)
-        // writeRoomJson(traversalGraph)
-        localStorage.setItem('graph', JSON.stringify(traversalGraph))
       }
-    }, 25000) // FIX - needs to dynamically handle current cooldown.
+
+      let direction;
+      if (unexploredExits.length > 0) {
+        direction = unexploredExits.pop();
+        stack.push(reverseDirection[direction]);
+      } else {
+        direction = stack.pop();
+      }
+
+      const moveRes = await axiosWithAuth().post(`${baseUrl}/api/adv/move/`, {
+        direction: direction
+      });
+      currentRoomID = moveRes.data.room_id;
+      if (!(currentRoomID in traversalGraph)) {
+        traversalGraph[currentRoomID] = moveRes.data;
+        for (let i = 0; i < moveRes.data.exits.length; i++) {
+          traversalGraph[currentRoomID][moveRes.data.exits[i]] = "?";
+        }
+      }
+      traversalGraph[prevRoomID][direction] = currentRoomID;
+      traversalGraph[currentRoomID][reverseDirection[direction]] = prevRoomID;
+
+      console.log("traversalGraph", traversalGraph);
+      localStorage.setItem("graph", JSON.stringify(traversalGraph));
+
+      await wait(moveRes.data.cooldown * 1000);
+    }
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
 }
 

380718809da7bb4f3332c873ee3ebe4d2eb4a516
Jeff Lapp <lappjeff20@gmail.com>
2019-10-23 10:11:38 -0400

    Started work on util functions

---
+++ client/src/helpers/util.js
@@ -0,0 +1,11 @@
+import map from './map'
+const wait = ms => {
+  return new Promise((res, rej) => setTimeout(res, ms));
+};
+
+const pickItem = (object, encumberance) => {
+
+}
+
+const triggerWiseExplorer => ()
+export { wait };

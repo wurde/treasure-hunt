df7e31e525b2a627f5399a758835d07bfba4df6d
Andy Bettisworth <andy@accreu.com>
2019-10-24 14:37:29 -0500

    Style the CheatCodes bar

--- client/src/components/cheat-codes/CheatCodes.js
+++ client/src/components/cheat-codes/CheatCodes.js
@@ -19,22 +19,25 @@ function CheatCodes() {
 	}
 
 	return (
-		<div className="CheatCodes pannel">
+		<div className="CheatCodes pannel mb-4">
 			<div className="translucent"></div>
 
-			<button onClick={traverseMap} style={{ margin: '5px' }}>Traverse Map</button>
-			<button onClick={askTravelTo} style={{ margin: '5px' }}>Go To Room</button>
-			<button onClick={e => travelTo(1)} style={{ margin: '5px' }}>Go To Shop</button>
-			<button onClick={e => travelTo(22)} style={{ margin: '5px' }}>Go To The Peak of Mt. Holloway</button>
-			<button onClick={e => travelTo(55)} style={{ margin: '5px' }}>Go To Wishing Well</button>
-			<button onClick={e => travelTo(461)} style={{ margin: '5px' }}>Go To Linh's Shrine</button>
-			<button onClick={e => travelTo(467)} style={{ margin: '5px' }}>Go To Pirate Ry's</button>
-			<button onClick={e => travelTo(495)} style={{ margin: '5px' }}>Go To The Transmogriphier</button>
-			<button onClick={e => travelTo(499)} style={{ margin: '5px' }}>Go To Glasowyn's Grave</button>
-			<button onClick={startGoldFarming} style={{ margin: '5px' }}>Start Gold Farming</button>
-			<button onClick={() => console.log(generatePath(499, 1))}>
-				Generate path
-			</button>
+			<div class="p-2">
+				<h3 className="text-black">Cheat Codes</h3>
+				<button onClick={askTravelTo} className="m-1 btn btn-sm btn-primary">Go To Room</button>
+				<button onClick={e => travelTo(1)} className="m-1 btn btn-sm btn-primary">Go To Shop</button>
+				<button onClick={e => travelTo(22)} className="m-1 btn btn-sm btn-primary">Go To The Peak of Mt. Holloway</button>
+				<button onClick={e => travelTo(55)} className="m-1 btn btn-sm btn-primary">Go To Wishing Well</button>
+				<button onClick={e => travelTo(461)} className="m-1 btn btn-sm btn-primary">Go To Linh's Shrine</button>
+				<button onClick={e => travelTo(467)} className="m-1 btn btn-sm btn-primary">Go To Pirate Ry's</button>
+				<button onClick={e => travelTo(495)} className="m-1 btn btn-sm btn-primary">Go To The Transmogriphier</button>
+				<button onClick={e => travelTo(499)} className="m-1 btn btn-sm btn-primary">Go To Glasowyn's Grave</button>
+				<button onClick={startGoldFarming} className="m-1 btn btn-sm btn-primary">Start Gold Farming</button>
+				<button onClick={traverseMap} className="m-1 btn btn-sm btn-primary">Traverse Map</button>
+				<button onClick={() => console.log(generatePath(499, 1))} className="m-1 btn btn-sm btn-primary">
+					Generate path
+				</button>
+			</div>
 		</div>
 	)
 }

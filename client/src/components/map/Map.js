4a34eeebe688ef62722d7ed0339d7d75ef78adf8
Jeff Lapp <lappjeff20@gmail.com>
2019-10-24 14:02:33 -0400

    Formatting changes and improved special room detection logic

--- client/src/components/map/Map.js
+++ client/src/components/map/Map.js
@@ -1,67 +1,72 @@
-import React from 'react';
-import './Map.scss';
+import React from "react";
+import "./Map.scss";
 
-import mapData from '../../helpers/map.json'
+import mapData from "../../helpers/map.json";
 
-let mapMatrix = []
-for (let i=0; i<29; i++) {
-	console.log('here')
-	let row = []
-	for (var j=0; j<32; j++) {
-		row.push(null)
-	}
-	mapMatrix.push(row)
+let mapMatrix = [];
+for (let i = 0; i < 29; i++) {
+  let row = [];
+  for (var j = 0; j < 32; j++) {
+    row.push(null);
+  }
+  mapMatrix.push(row);
 }
 
 for (var room in mapData) {
-	let x = parseInt(mapData[room].coordinates.slice(1,3))
-	let y = parseInt(mapData[room].coordinates.slice(4,6))
-	mapMatrix[y-46][x-46] = mapData[room]
+  let x = parseInt(mapData[room].coordinates.slice(1, 3));
+  let y = parseInt(mapData[room].coordinates.slice(4, 6));
+  mapMatrix[y - 46][x - 46] = mapData[room];
 }
 
-mapMatrix.reverse()
+mapMatrix.reverse();
 
-console.log(mapMatrix)
-console.log(mapData[177])
+let classes = "";
 
-let classes = ''
+let specialRooms = {
+  shrines: new Set([22, 461, 499]),
+  well: 55,
+  shop: 1,
+  pirateRy: 467,
+  transmogri: 495
+};
 
 function hasExits(room) {
-	classes = 'room'
-	if (room) {
-		room.exits.includes('n') && (classes += ' north')
-		room.exits.includes('e') && (classes += ' east')
-		room.exits.includes('s') && (classes += ' south')
-		room.exits.includes('w') && (classes += ' west')
-		room.description.includes('well') && (classes += ' well')
-		room.description.includes('shrine') && (classes += ' shrine')
-		room.description.includes('shop') && (classes += ' shop')
-		room.title.includes('Transmogri') && (classes += ' transmogri')
-		room.description.includes('Pirate') && (classes += ' pirate')
-		room.room_id === 356 && (classes += ' mine')
-		room.items.length > 0 && (classes += ' treasure')
-	} else {
-		classes = 'noroom'
-	}
-	return classes
+  classes = "room";
+
+  if (room) {
+    let { exits, description, title, room_id, items } = room;
+
+    exits.includes("n") && (classes += " north");
+    exits.includes("e") && (classes += " east");
+    exits.includes("s") && (classes += " south");
+    exits.includes("w") && (classes += " west");
+    specialRooms.well === room_id && (classes += " well");
+    specialRooms.shrines.has(room_id) && (classes += " shrine");
+    specialRooms.shop === room_id && (classes += " shop");
+    specialRooms.transmogri === room_id && (classes += " transmogri");
+    specialRooms.pirateRy === room_id && (classes += " pirate");
+    // room.room_id === 356 && (classes += " mine");
+    items.length > 0 && (classes += " treasure");
+  } else {
+    classes = "noroom";
+  }
+  return classes;
 }
 
 function Map() {
   return (
     <div className="Map pannel">
-    	<div className="translucent"></div>
+      <div className="translucent"></div>
 
-		<div className="map-display">
-			{mapMatrix.map(row => (
-				<div className="row">
-					{row.map(room => (
-						<div className={hasExits(room)}>
-							{room && room.room_id}
-						</div>
-					))}
-				</div>
-			))}
-		</div>
+      <div className="map-display">
+        {mapMatrix.map(row => (
+          <div className="row">
+            {row.map(room => (
+              <div className={hasExits(room)}>{room && room.room_id}</div>
+            ))}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }

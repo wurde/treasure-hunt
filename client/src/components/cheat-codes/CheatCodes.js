8ae0a570894c0e05cff3d69bcf87637a8a2bf482
Jeff Lapp <lappjeff20@gmail.com>
2019-10-24 23:12:02 -0400

    Updated CheatCodes
    - Added proper room logging for each traverseMap function
    - Add Wishing Well button

--- client/src/components/cheat-codes/CheatCodes.js
+++ client/src/components/cheat-codes/CheatCodes.js
@@ -13,53 +13,62 @@ import { generatePath } from "../../helpers/util";
  * Define component
  */
 
-function CheatCodes({ generateMovementMessage }) {
+function CheatCodes({ generateMovementMessage, getRoomData }) {
   async function askTravelTo() {
     const roomID = window.prompt("Room ID:");
 
-    generateMovementMessage(roomID);
+    await generateMovementMessage(roomID);
 
-    travelTo(roomID);
+    travelTo(roomID, getRoomData);
   }
 
   return (
     <div className="CheatCodes pannel mb-4">
       <div className="translucent"></div>
 
-      <div class="p-2">
+      <div className="p-2">
         <h3 className="text-black">Cheat Codes</h3>
         <button onClick={askTravelTo} className="m-1 btn btn-sm btn-primary">
           Go To Room
         </button>
         <button
-          onClick={e => travelTo(1)}
+          onClick={e => travelTo(1, getRoomData)}
           className="m-1 btn btn-sm btn-primary"
         >
           Go To Shop
         </button>
         <button
-          onClick={e => travelTo(467)}
+          onClick={e => travelTo(467, getRoomData)}
           className="m-1 btn btn-sm btn-primary"
         >
           Go To Pirate Ry's
         </button>
         <button
-          onClick={e => travelTo(495)}
+          onClick={e => travelTo(495, getRoomData)}
           className="m-1 btn btn-sm btn-primary"
         >
           Go To The Transmogriphier
+        </button>
+        <button
+          onClick={e => travelTo(55, getRoomData)}
+          className="m-1 btn btn-sm btn-primary"
+        >
+          Go To Wishing Well
         </button>
         <button onClick={earnNewPowers} className="m-1 btn btn-sm btn-primary">
           Earn New Powers
         </button>
         <button
-          onClick={startGoldFarming}
+          onClick={() => startGoldFarming(getRoomData)}
           className="m-1 btn btn-sm btn-primary"
         >
           Start Gold Farming
         </button>
         {/* <button onClick={examineWishingWell} className="m-1 btn btn-sm btn-primary">Examine Wishing Well</button> */}
-        <button onClick={traverseMap} className="m-1 btn btn-sm btn-primary">
+        <button
+          onClick={() => traverseMap(getRoomData)}
+          className="m-1 btn btn-sm btn-primary"
+        >
           Traverse Map
         </button>
       </div>
